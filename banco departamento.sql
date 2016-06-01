7b5fc14ef11f62f564ddb0cf0d1ca481cba0ceff
filipe duarte <filipeduarte.si@gmail.com>
2016-06-01 00:23:51 -0300

    adicionei o banco

---
+++ banco departamento.sql
@@ -0,0 +1,14 @@
+﻿create table professor (id serial, 
+			nome varchar(50),
+			siape int,
+			ramal int,
+			celular int,
+			sala varchar(20),
+			cpf int,
+			identidade int, 
+			email varchar(50),
+			primary key(id));
+
+select*from professor
+
+drop table professor
